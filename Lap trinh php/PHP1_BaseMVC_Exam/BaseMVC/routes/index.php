e3d04eb4493bb4b795fa58a0d8c95313688e332a
IIlII1 <86063682+IIlII1@users.noreply.github.com>
2026-07-29 19:16:03 +0700

    Delete Lap trinh php directory

--- Lap trinh php/PHP1_BaseMVC_Exam/BaseMVC/routes/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$action = $_GET['action'] ?? '/';
-
-match ($action) {
-    '/'         => (new HomeController)->index(),
-    'add'  => (new HomeController)->add(),
-    'stor' => (new HomeController)->stor(),
-};
